961787fa58f2a3c3f35b416da88666a7c2c7e6d8
Kate Muraya <murayakate@gmail.com>
2025-11-02 20:07:03 +0300

    fix(Header): restore default export to Header.jsx
    The app rendered a blank page due to an incorrect/missing export. Replaced Header implementation with a proper functional component and export. no longer shows errors

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,13 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
+import Header from './Header.jsx'
+import MainContent from './MainContent.jsx'
+import Footer from './Footer.jsx'
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
