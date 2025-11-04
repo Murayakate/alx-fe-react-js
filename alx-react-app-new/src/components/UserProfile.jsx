5917a32328381b3930059d096eb20c97173be4c1
Kate Muraya <murayakate@gmail.com>
2025-11-04 16:03:55 +0300

    added css styling to header jsx user profile failed

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 const UserProfile = (props) => {
    return (
-     <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-     <h2 style={{ color: 'yellow' }}>{props.name}</h2>
-     <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-     <p> Bio: <span style={{ fontWeight: 'bold' }}>{props.bio}</span></p>
+     <div style={{color: 'blue', border: '1px solid gray', padding: '10px', margin: '10px' }}>
+     <h2 style={{ color: 'blue' }}>{props.name}</h2>
+     <p>Age: <span style={{color: 'blue', fontWeight: 'bold' }}>{props.age}</span></p>
+     <p> Bio: <span style={{ color: 'green', fontWeight: 'bold' }}>{props.bio}</span></p>
      </div>
    );
  };
